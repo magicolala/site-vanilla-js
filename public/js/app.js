6cf0fef14f876b74f827d1f7a01d3bbf11b337d2
Cédric Oloa <cedric.oloa@laposte.net>
2018-06-19 14:06:23 +0200

    templates

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const el = $('#app');
+
+  // Compile Handlebar Templates
+  const errorTemplate = Handlebars.compile($('#error-template').html());
+  const ratesTemplate = Handlebars.compile($('#rates-template').html());
+  const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
+  const historicalTemplate = Handlebars.compile($('#historical-template').html());
+
+  const html = ratesTemplate();
+  el.html(html);
+});
